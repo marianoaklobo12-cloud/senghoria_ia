067f9df9d18c74dcfa7fbddbb25dd929a70141f4
marianoaklobo12-cloud <marianoaklobo12@gmail.com>
2026-08-10 16:48:32 +0100

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "senghor-ia-v1";
+
+const fichiers = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(fichiers);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
